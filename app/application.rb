532595abd235ae91d78af55561b598b92f9d7fdd
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-18 22:21:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -29,6 +29,7 @@ class Application
         @@cart << item
         resp.write "added #{item}"
       else
+        resp.write "We don't have that item!"
       end
     else
       resp.write "Path Not Found"
